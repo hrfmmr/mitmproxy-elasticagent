7d13dc548fd0b93f27a0572711c9986db9b0b666
hrfmmr <i@hrfmmr.com>
2021-11-22 18:02:38 +0900

    fixup! [wip] Add dumping open api spec yaml script

--- dumper/oasdumper/parser/__init__.py
+++ dumper/oasdumper/parser/__init__.py
@@ -1,3 +1,8 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
 class OASParser:
     @staticmethod
     def gettype(type):
@@ -6,7 +11,7 @@ class OASParser:
         for i in ["string", "boolean", "integer"]:
             if type in i:
                 return i
-        assert f"unexpected type:{type}"
+        return None
 
     @staticmethod
     def parse(json_data):
@@ -14,8 +19,12 @@ class OASParser:
         if type(json_data) is dict:
             d["type"] = "object"
             d["properties"] = {}
-            for key in json_data:
-                d["properties"][key] = OASParser.parse(json_data[key])
+            for k in json_data:
+                c = OASParser.parse(json_data[k])
+                if not c:
+                    logger.warning(f"🚨 parsing type failed for key:{k}")
+                    continue
+                d["properties"][k] = c
             return d
         elif type(json_data) is list:
             d["type"] = "array"
@@ -25,7 +34,10 @@ class OASParser:
                 d["items"] = "object"
             return d
         else:
-            d["type"] = OASParser.gettype(type(json_data).__name__)
+            t = OASParser.gettype(type(json_data).__name__)
+            if not t:
+                return None
+            d["type"] = t
             if d["type"] == "number":
                 d["format"] = "float"
             return d
